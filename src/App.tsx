cd53dd207d32165956d2b8d259e4449af2b332fe
AviadSofer <aviadsofer@gmail.com>
2022-01-27 16:05:41 +0200

    basic

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      react and TS
+    </div>
+  )
+}
+
+export default App
