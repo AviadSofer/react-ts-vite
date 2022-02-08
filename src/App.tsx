2e255234bb290d18a06484d3f13909a95f50738c
AviadSofer <aviadsofer@gmail.com>
2022-02-08 23:12:21 +0200

    remove theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { NumberProvider } from './NumberContext';
 import { StyledApp } from './components/styles/App,styled';
 import GlobalStyles from './components/styles/Global';
 import Container from './components/Container';
-import MessageView from './components/MessageView';
+import MessageView from './components/MessageView'
 
 const App: React.FC = () => {
   return (
